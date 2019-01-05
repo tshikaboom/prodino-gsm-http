b1fd6442f0a7bb6b725678993234f5abf766d543
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2019-01-05 16:42:32 +0200

    sms: Minimally implement SMS sending
    This has not been tested.
    
    Signed-off-by: Oskar Viljasaar <oskar.viljasaar@gmail.com>

--- sms.ino
+++ sms.ino
@@ -2,6 +2,8 @@
 
 #include <TinyGsmClient.h>
 
+#define SMS_JSON_BUF_SIZE 320
+
 void http_sms_get(EthernetClient client) {
   current_response.value = -ENOSYS;
 }
@@ -11,7 +13,33 @@ void http_sms_get(EthernetClient client) {
    This only accepts international format numbers, prefixed with a +.
 */
 void http_sms_post(EthernetClient client, PostParser http_data) {
-  current_response.value = -ENOSYS;
+  StaticJsonBuffer<SMS_JSON_BUF_SIZE * 2> input_buffer;
+  JsonObject& root = input_buffer.parseObject(http_data.getPayload());
+  unsigned int sms_target_index = http_data.getHeader().indexOf("+");
+  String sms_target = http_data.getHeader().substring(sms_target_index);
+  int ret;
+
+  if (!root.success()) {
+    PR_DEBUGLN("JSON parse failed!");
+    current_response.value = -EINVAL;
+    return;
+  }
+
+  const char* sms_contents = root["message"];
+
+  PR_DEBUG("Sending message \"");
+  PR_DEBUG(sms_contents);
+  PR_DEBUG("\" to ");
+  PR_DEBUG(sms_target);
+
+  ret = modem.sendSMS(sms_target, sms_contents);
+
+  PR_DEBUG(": ");
+  PR_DEBUGLN(ret ? "OK." : "failed.");
+
+  // let's use EIO to indicate SMS sending fail
+  current_response.value = ret ? 0 : -EIO;
+  return;
 }
 
 // Not implemented.
@@ -37,9 +65,12 @@ void http_sms_request(EthernetClient client, PostParser http_data) {
     case 0:
       accept_connection(client);
       break;
-      
+
+    case -EINVAL:
     case -ENOSYS:
+    case -EIO:
     default:
+      error_connection(client);
       break;
   }
 }
