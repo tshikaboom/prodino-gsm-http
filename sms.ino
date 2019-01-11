d3416bb3f091c75439600e43ec158c027bc6b886
Oskar Viljasaar <oskar.viljasaar@gmail.com>
2019-01-11 10:36:00 +0200

    sms: blindly fix up GET, subject to testing
    Signed-off-by: Oskar Viljasaar <oskar.viljasaar@gmail.com>

--- sms.ino
+++ sms.ino
@@ -5,19 +5,27 @@
 #include <TinyGsmClient.h>
 
 #define SMS_JSON_BUF_SIZE 320
-
+/*
+  AT reference:
+  AT+CMGL="ALL"
+  +CMGL: 1,"REC UNREAD","+31628870634",,"11/01/09,10:26:26+04"
+  This is text message 1
+  +CMGL: 2,"REC UNREAD","+31628870634",,"11/01/09,10:26:49+04"
+  This is text message 2
+  OK
+*/
 void http_sms_get(EthernetClient client) {
   int ret;
-  int number_end;
   String local_buffer(received_chars);
   String iterator;
   String number;
   String sms_contents;
   int i = 0;
   char *it;
-  char *number_begin, *number_end, *real_number;
+  int number_begin, number_end;
+  int sms_begin, sms_end;
 
-  SerialGSM.println("AT+CMGL=ALL");
+  SerialGSM.println("AT+CMGL=\"ALL\"");
 
   modem_getResponse();
 
@@ -26,31 +34,37 @@ void http_sms_get(EthernetClient client) {
   while ((it = strstr(received_chars, "+CMGL")) != NULL) {
     String s = String(it);
     s.trim();
-    if ((number = strstr(it, "\"+")) != NULL) {
+    if ((number_begin = s.indexOf("\"+")) != -1) {
+      if ((number_end = s.substring(number_begin).indexOf("\",")) != -1) {
+        // we've got a number
+        number_end--; // move to the last character of the number
+        number_begin++; // move to the '+' number prefix
+        number = s.substring(number_begin, number_begin + number_end);
+        PR_DEBUG("SMS from number ");
+        PR_DEBUG(number);
+        sms_begin = s.indexOf("\r\n");
+        if (sms_begin != -1) {
+          sms_begin += 2;
+          sms_end = s.substring(sms_begin).indexOf("\r\n");
+          sms_contents = s.substring(sms_begin, sms_begin + sms_end);
+          PR_DEBUG(": ");
+          PR_DEBUG(" \"");
+          PR_DEBUG(sms_contents);
+          PR_DEBUGLN("\"");
 
+          if (i > 0)
+            strcat(current_response.body, ",");
+
+          strcat(current_response.body, "\"from\":\"");
+          strcat(current_response.body, number.c_str());
+          strcat(current_response.body, "\", \"message\":\"");
+          strcat(current_response.body, sms_contents.c_str());
+          strcat(current_response.body, "\"");
+          i++;
+        }
+      }
     }
   }
-  while (iterator = local_buffer.substring(local_buffer.indexOf("+")) != "") {
-    number_end = iterator.indexOf("\"");
-    number = iterator.substring(0, number_end);
-    PR_DEBUG("SMS from number ");
-    PR_DEBUG(number);
-    sms_contents = iterator.indexOf("\\r\\n") + 2;
-    PR_DEBUG(" \"");
-    PR_DEBUG(sms_contents);
-    PR_DEBUGLN("\"");
-
-    if (i > 0)
-      strcat(current_response.body, ",");
-
-    strcat(current_response.body, "\"from\":\"");
-    strcat(current_response.body, number.c_str());
-    strcat(current_response.body, "\", \"message\":\"");
-    strcat(current_response.body, sms_contents.c_str());
-    strcat(current_response.body, "\"");
-    i++;
-  }
-
   strcat(current_response.body, "]}");
 
   current_response.value = 0;
